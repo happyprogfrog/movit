8318c3753fc792494fcb6a760a5c2784decdb681
happyprogfrog <happyprogfrog@gmail.com>
2024-05-29 09:30:11 +0900

    refactor: 리스트 변환 시 java 16의 더 간결한 메서드로 변경

--- src/main/java/com/happyprogfrog/movit/service/MovieService.java
+++ src/main/java/com/happyprogfrog/movit/service/MovieService.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -28,7 +27,7 @@ public class MovieService {
         return movieRepository.findAll()
                 .stream()
                 .map(MovieDefaultDto::new)
-                .collect(Collectors.toList());
+                .toList();
     }
 
     /**
